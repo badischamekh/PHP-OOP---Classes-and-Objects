3068e562580e02d0cba5d0b2fec179e0bf653ec8
badis chamekh <badischmk@gmail.com>
2021-04-03 08:24:42 +0200

    first commit

---
+++ utils/footer.php
@@ -0,0 +1,36 @@
+<hr class = "footerline"><!--css modified horizontal line-->
+<footer>
+    <div class = "container">
+        <div class = "row">
+            <section>
+                <div class = "footerContent col-md-4"><!--left content-->
+                    <p class = "footerContent1">
+                        <strong>D</strong><span class = "small footerSubtext">esign and</span>
+                        <strong>T</strong><span class = "small footerSubtext">echnology</span>
+                    </p>
+
+                    <p class = "footerSubtext2">
+                        Ariana ariana Tunis
+                        &copy; Urban , 2021.
+                    </p>
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--middle content-->
+                    Engineering<br>
+                    Creative Computing 2020-2021.<br>
+                    Ariana tunisie, N00145647
+                </div>
+            </section>
+            <section>
+                <div class = "footcontent col-md-4"><!--right content-->
+                    Follow Us:<br>
+                        <img src = "images/facebook.png">
+                        <img src = "images/twitter.png">
+                        <img src = "images/googleplus.png">
+                        <img src = "images/youtube.png">
+                </div>
+            </section>
+        </div>
+    </div>
+</footer>
